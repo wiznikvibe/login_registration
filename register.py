966e835fc91fbda0252a1ee7a8514bd94f67e742
wiznikvibe <nikhilshetty439@gmail.com>
2023-04-29 02:28:48 +0530

    Frame Assigned

--- register.py
+++ register.py
@@ -10,6 +10,14 @@ class Register:
         self.root.title('Registration Form')
         self.root.geometry("1080x720+0+0")
 
+        self.back = ImageTk.PhotoImage(file='images/register.jpg')
+        reg_bg = Label(self.root, image = self.back)
+        reg_bg.place(x=0,y=0,relwidth=1,relheight=1)
+        
+
+        frame = Frame(self.root,bg='white',highlightthickness=0)
+        frame.place(x=200,y=150,width=650,height=450)
+
 if __name__ == '__main__':
     root = Tk()
     app = Register(root)
