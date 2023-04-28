fb1827a3099f2f552feec14dc16367d0a098a7d5
wiznikvibe <nikhilshetty439@gmail.com>
2023-04-28 20:42:13 +0530

    changes made

--- login.py
+++ login.py
@@ -22,14 +22,21 @@ class Login_window:
         lab_img = Label(image=self.log_img,bg='white',borderwidth=5)
         lab_img.place(x=225,y=85)
 
-        log_in = Label(frame, text="Login", font=('Arial',11,'bold'),bg='white')
-        log_in.place(x=73,y=35)
-        user_lab = Label(frame, text="Username:", font=('Arial',8,'normal'),bg='white')
+        log_in = Label(frame, text="Login", font=('Arial',17,'bold'),bg='white')
+        log_in.place(x=70,y=35)
+
+        user_lab = Label(frame, text="Username:", font=('Arial',10,'normal'),bg='white')
         user_lab.place(x=10, y=75)
-        pass_lab = Label(frame, text="Password:", font=('Arial',8,'normal'),bg='white')
-        pass_lab.place(x=10, y=95)
-        user_entry = Entry(frame, font=('Arial',8,'normal'),bg='white')
-        user_entry.place(x=70,y=75, width=95)
+
+        
+        pass_lab = Label(frame, text="Password:", font=('Arial',10,'normal'),bg='white')
+        pass_lab.place(x=10, y=125)
+        
+        
+        user_entry = Entry(frame, font=('Arial',10,'normal'),bg='white')
+        user_entry.place(x=1,y=95, width=182)
+        pass_entry = Entry(frame, font=('Arial',10,'normal'),bg='white')
+        pass_entry.place(x=1,y=145, width=182)
 
 
 
